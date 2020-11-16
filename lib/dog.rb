c4fc2508abfd5e7eafb8aab8e1933ddd9991a354
Wesley <wspencer3295@yahoo.com>
2020-11-16 14:17:23 -0700

    commit tree 0b707094f6a36c7a9d743da73900189e344ab563

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+
+def initialize (name ,breed = "Mutt")
+    @name = name
+    @breed = breed
+    
+end
+
+def name=(name , breed)
+    @name = name
+    @breed = breed
+end
+
+def name_breed 
+    @name
+    @breed
+end
+
+
+end
+
